07561f51c9d1184006b70982516689a3363c8d9b
warmth27 <144099301+warmth27@users.noreply.github.com>
2024-09-20 16:50:05 +0800

    Update load.py

--- load.py
+++ load.py
@@ -1,39 +1,43 @@
+"""
+This is just a quick script that is able to load the files. Just using pandas can be tricky because of the newline characters in the text data. Here it is handled via the `parse_col` method.
+"""
+
 import ast
 import os
+
 import pandas as pd
 
-our_dataset_path = 'dataset'
-output_json_path = 'dataset/'  # 设置输出JSON的路径
+our_dataset_path = '.'
 
-posts_path = os.path.join(our_dataset_path, 'posts.csv')
-fact_checks_path = os.path.join(our_dataset_path, 'fact_checks.csv')
-fact_check_post_mapping_path = os.path.join(our_dataset_path, 'pairs.csv')
+posts_path = os.path.join(our_dataset_path, 'trial_posts.csv')
+fact_checks_path = os.path.join(our_dataset_path, 'trial_fact_checks.csv')
+fact_check_post_mapping_path = os.path.join(our_dataset_path, 'trial_data_mapping.csv')
 
-# 确保每个文件都存在
 for path in [posts_path, fact_checks_path, fact_check_post_mapping_path]:
-    assert os.path.isfile(path), f"File not found: {path}"
+    assert os.path.isfile(path)
 
-# 预处理文本字段，处理换行问题
+# We need to apply t = t.replace('\n', '\\n') for text fields before using `ast.literal_eval`.
+# `ast.literal_eval` has problems when there are new lines in the text, e.g.:
+# `ast.literal_eval('("\n")')` effectively tries to interpret the following code:
+
+# ```
+# ("
+# ")
+# ```
+
+# This raises a SyntaxError exception. By escaping new lines we are able to force it to interpret it properly. There might
+# be some other way to do this more systematically, but it is a workable fix for now.
+
 parse_col = lambda s: ast.literal_eval(s.replace('\n', '\\n')) if s else s
 
-# 读取并处理事实核查数据
 df_fact_checks = pd.read_csv(fact_checks_path).fillna('').set_index('fact_check_id')
-for col in ['fact_check_id', 'claim', 'instances', 'title']:
+for col in ['claim', 'instances', 'title']:
     df_fact_checks[col] = df_fact_checks[col].apply(parse_col)
 
-# 读取并处理帖子数据
+
 df_posts = pd.read_csv(posts_path).fillna('').set_index('post_id')
 for col in ['instances', 'ocr', 'verdicts', 'text']:
     df_posts[col] = df_posts[col].apply(parse_col)
 
-# 读取映射数据
-df_fact_check_post_mapping = pd.read_csv(fact_check_post_mapping_path)
 
-# 确保输出路径存在
-if not os.path.exists(output_json_path):
-    os.makedirs(output_json_path)
-
-# 保存数据到JSON文件
-df_fact_checks.to_json(os.path.join(output_json_path, 'fact_checks.json'), orient='records', index)
-df_posts.to_json(os.path.join(output_json_path, 'posts.json'), orient='records', lines=True)
-df_fact_check_post_mapping.to_json(os.path.join(output_json_path, 'fact_check_post_mapping.json'), orient='records', lines=True)
+df_fact_check_post_mapping = pd.read_csv(fact_check_post_mapping_path) 
